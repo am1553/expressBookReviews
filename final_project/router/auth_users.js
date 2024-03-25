d7d57d241c05d3cb2985c16959a9a930e34ed8f3
Aryan Mehta <aryanmehta2016@gmail.com>
2024-03-25 23:00:29 +0000

    task 14

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,67 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => {
+  const user = users.find(u => u.username === username);
+  return user ? true : false;
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+  const user = users.find(u => u.username === username);
+  const userToken = user.token;
+  const verifyToken = jwt.verify(userToken, "access");
+  return verifyToken ? true : false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const {username, password} = req.body;
+  if(!isValid(username)) {
+    return res.status(400).json({message: "User doesn't exist."})
+  }
+
+  if(authenticatedUser(username, password)) {
+    const user = users.find(u => u.username === username);
+    req.session.authorization = {accessToken: user.token};
+    return res.status(200).json({message: "Successfully logged in.", user})
+  }
+
+  return res.status(400).json({message: "Invalid credentials."});
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const {isbn} = req.params;
+  const {review: reviewStr} = req.query;
+  const token = req.session.authorization['accessToken']
+  const {username} = users.find(u => u.token === token);
+
+  for(i in books[isbn].reviews) {
+    if(books[isbn].reviews[i].user === username) {
+      books[isbn].reviews[i] = {user: username, review: reviewStr};
+      return res.status(200).json({message: "Updated", books})
+    }
+  }
+  const reviewsLength = Object.keys(books[isbn].reviews).length;
+  books[isbn].reviews[reviewsLength] = {user: username, review: reviewStr};
+  return res.status(200).json({message: "Updated", books});
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+
+  const {isbn} = req.params;
+  const token = req.session.authorization['accessToken']
+  const {username} = users.find(u => u.token === token);
+
+  for(i in books[isbn].reviews) {
+    if(books[isbn].reviews[i].user === username) {
+      books[isbn].reviews = {};
+    }
+  }
+  return res.status(200).json({message: "Deleted", books});
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,42 +2,96 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+const jwt = require('jsonwebtoken');
 const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const {username, password} = req.body;
+  if(isValid(username)) {
+    return res.status(400).json({message: "username already exists."})
+  }
+  if(!username || !password) {
+    return res.status(400).json({message: "username and password is required."})
+  }
+
+  const token = jwt.sign({username, password}, "access")
+  users.push({username, password, token});
+
+  return res.status(200).json({message: "User registered.", users});
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', async function (req, res) {
+  const booksPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(books)
+    },6000)
+  })
+  const bks = await booksPromise;
+  return res.status(200).json({books: bks});
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+public_users.get('/isbn/:isbn', async function (req, res) {
+  const {isbn} = req.params;
+  const bookPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(books[isbn])
+    },6000)
+  })
+  const book = await bookPromise;
+
+  return res.status(200).json({book});
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author',async function (req, res) {
+  const {author} = req.params;
+  const bookPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      for(i in books) {
+        const bk = books[i];
+        if(bk.author === author) {
+          resolve(bk)
+        }
+      }
+
+    },6000)
+  })
+
+  const book = await bookPromise;
+
+  return res.status(200).json(book);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title',async function (req, res) {
+  const {title} = req.params;
+  const bookPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      for(i in books) {
+        const bk = books[i];
+        if(bk.title === title) {
+          resolve(bk);
+        }
+      }
+    },6000)
+  })
+
+  const book = await bookPromise
+
+
+  return res.status(200).json(book);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const {isbn} = req.params;
+  const book = books[isbn];
+  return res.status(200).json(book.reviews);
 });
 
 module.exports.general = public_users;
